cc2d8890ddef87bbf85faab807ae0a75477b3688
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2023-02-28 01:02:48 +0530

    finalizing the project, added curriculum filter dropdown, added tags array to courses, updated general css

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,6 +21,7 @@ const Main = () => {
                     webpage
                     id
                     heroImage
+                    tag
                 }
             }
         }
@@ -34,29 +35,33 @@ const Main = () => {
     const [repo, setRepo]= useState({checkbox1:true, checkbox2:true});
     const [webpage, setWebpage]= useState({checkbox1:true, checkbox2:true});
     const [status, setStatus]= useState("all");
+    const [curriculum, setCurriculum]= useState("all");
     
     return (
         <div className='main'>
             <div className="filters">
                 <h2>Filter Content</h2>
                 <div className='allFilters'>
-                    <div className="statusFilter"><Dropdown data="status" types={["all", "stable", "beta", "alpha"]} state={status} setState={setStatus}/></div>
-                    <div className="videoFilter"><CheckBox data="videos" state={videos} setState={setVideos}/></div>
-                    <div className="repoFilter"><CheckBox data="repository" state={repo} setState={setRepo}/></div>
-                    <div className="webpageFilter"><CheckBox data="webPage" state={webpage} setState={setWebpage}/></div>
+                    <div><Dropdown data="curriculum" types={["all", "Software_Basics", "Software_Development", "Frontend", "Backend"]} state={curriculum} setState={setCurriculum}/></div>
+                    <div><Dropdown data="status" types={["all", "stable", "beta", "alpha"]} state={status} setState={setStatus}/></div>
+                    <div><CheckBox data="videos" state={videos} setState={setVideos}/></div>
+                    <div><CheckBox data="repository" state={repo} setState={setRepo}/></div>
+                    <div><CheckBox data="webPage" state={webpage} setState={setWebpage}/></div>
                 </div>
             </div>
             <div className="content">
                 <div className="searchBox">
                     <input type="text" placeholder="Search" value={searchTxt} onChange={(e)=>{setSearchTxt(e.target.value)}}/>
                 </div>
-                {courses.map((course)=>{
+                {courses.map((course) => {
                     if((searchTxt.toLowerCase === "") || (course.node?.name?.toLowerCase().includes(searchTxt.toLowerCase())) || (course.node?.description?.toLowerCase().includes(searchTxt.toLowerCase())) || (course.node?.status?.toLowerCase().includes(searchTxt.toLowerCase()))){
                         if((status === "all") || (course.node?.status === status)){
-                            if((videos.checkbox1 === true && videos.checkbox2 === true) || (videos.checkbox1 === true ? course.node?.videos !== "" : course.node?.videos === "") && (videos.checkbox2 === true ? course.node?.videos === "" : course.node?.videos !== "") ||
-                            (repo.checkbox11 === true && repo.checkbox2 === true) || (repo.checkbox1 === true ? course.node?.repository !== "" : course.node?.repository === "") && (repo.checkbox2 === true ? course.node?.repository === "" : course.node?.repository !== "") ||
-                            (webpage.checkbox1 === true && webpage.checkbox2 === true) || (webpage.checkbox1 === true  ? course.node?.webpage !== "" : course.node?.webpage === "") && (webpage.checkbox2 === true  ? course.node?.webpage === "" : course.node?.webpage !== ""))
-                                return <Card data={course} key={course.node?.id} /> 
+                            if((curriculum === "all") || (course.node?.tag?.includes(curriculum))){
+                                if((videos.checkbox1 === true && videos.checkbox2 === true) || (videos.checkbox1 === true ? course.node?.videos !== "" : course.node?.videos === "") && (videos.checkbox2 === true ? course.node?.videos === "" : course.node?.videos !== "") ||
+                                (repo.checkbox11 === true && repo.checkbox2 === true) || (repo.checkbox1 === true ? course.node?.repository !== "" : course.node?.repository === "") && (repo.checkbox2 === true ? course.node?.repository === "" : course.node?.repository !== "") ||
+                                (webpage.checkbox1 === true && webpage.checkbox2 === true) || (webpage.checkbox1 === true  ? course.node?.webpage !== "" : course.node?.webpage === "") && (webpage.checkbox2 === true  ? course.node?.webpage === "" : course.node?.webpage !== ""))
+                                    return <Card data={course} key={course.node?.id} /> 
+                            }
                         }
                     }
                 })}
